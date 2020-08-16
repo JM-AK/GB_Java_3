f0be2cf87fc95beeaa0072864a36f39db7ff6647
JM-AK <komlevalexandr87@gmail.com>
2020-08-16 16:40:26 +0300

    [Lesson-6] DZ - ArrayTests

--- Geekbrains-java2-master/stage-3/stage-3-lesson-6-hw/src/main/java/ru/gb/ArrayTestClass.java
+++ Geekbrains-java2-master/stage-3/stage-3-lesson-6-hw/src/main/java/ru/gb/ArrayTestClass.java
@@ -9,8 +9,30 @@ public class ArrayTestClass {
 
     public static void main(String[] args) {
 
+        System.out.println(checkContainNumsOneAndFour(new int[]{1,1,1,4,4}));
+
     }
 
+    public int [] getArrAfterLastFour (int [] array) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == 4) arrayList.clear();
+            else arrayList.add(array[i]);
+        }
+        if (arrayList.size() == array.length) throw new RuntimeException();
 
+        return arrayList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static boolean checkContainNumsOneAndFour(int[] array){
+        int x1 = 0;
+        int x4 = 0;
+
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == 1) x1++;
+            if(array[i] == 4) x4++;
+        }
+        return x1 > 0 && x4 > 0 && x1 + x4 == array.length;
+    }
 
 }
